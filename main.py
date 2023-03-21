5f602de134577304a3089a716d77ef4898725a5d
Goldman <goldmanmine@mail.ru>
2023-03-21 17:17:49 +0300

    Пейзажи Марса

--- main.py
+++ main.py
@@ -3,40 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return 'Миссия Колонизация Марса'
-
-
-@app.route('/index')
-def index():
-    return 'И на Марсе будут яблони цвести!'
-
-
-@app.route('/promotion')
-def promotion():
-    return '</br>'.join('''Человечество вырастает из детства.
-Человечеству мала одна планета.
-Мы сделаем обитаемыми безжизненные пока планеты.
-И начнем с Марса!
-Присоединяйся!'''.split('\n'))
-
-
-@app.route('/image_mars')
-def image_mars():
-    with open('image_mars.html', encoding='utf-8') as html:
-        return html.read()
-
-
-@app.route('/promotion_image')
-def promotion_image():
-    with open('promotion_image.html', encoding='utf-8') as html:
-        return html.read()
-
-
-@app.route('/astronaut_selection')
-def astronaut_selection():
-    with open('astronaut_selection.html', encoding='utf-8') as html:
+@app.route('/carousel')
+def carousel():
+    with open('carousel.html', encoding='utf-8') as html:
         return html.read()
 
 
